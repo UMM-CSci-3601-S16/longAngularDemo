7c777ad5a67db3430e96d697e25b4a5ee48a9119
thele116 <thele116@morris.umn.edu>
2016-02-03 14:00:04 -0600

    Added text field data binding expiriment.

--- main.controller.js
+++ main.controller.js
@@ -1,3 +1,8 @@
 angular.module("main").controller("mainController", function() {
     var self = this;
+    self.text = "stuff";
+
+    self.count=function(){
+        return self.text.length;
+    };
 });
